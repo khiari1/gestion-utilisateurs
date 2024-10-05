cea6801faa1b81c59764d5e0b04c678a3509a4d7
khiari-aymen <khiari.aymen.1@esprit.tn>
2024-10-05 22:51:12 +0100

    Fixedd

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async rewrites() {
-      return [
-        {
-          source: '/auth/google',
-          destination: '/:path*'  // Permet à Next.js de gérer toutes les routes
-        }
-      ];
-    }
-  };
-  
-  export default nextConfig;
-  
+  async rewrites() {
+    return [
+      {
+        source: '/auth/google', // Le chemin d'origine que vous voulez réécrire
+        destination: '/auth/google' // La destination, qui peut être la même ou différente
+      },
+      {
+        source: '/:path*', // Cette règle va s'appliquer à tous les chemins
+        destination: '/:path*' // Permet à Next.js de gérer toutes les routes
+      }
+    ];
+  }
+};
+
+export default nextConfig;
